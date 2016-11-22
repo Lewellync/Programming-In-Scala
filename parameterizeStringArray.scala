adbcb7b3d915783b4e581661032ce8c6f56b96eb
Connor Lewellyn <lewellync@gmail.com>
2016-11-22 12:39:17 -0500

    Everything is a method call. 3.7

--- parameterizeStringArray.scala
+++ parameterizeStringArray.scala
@@ -1,9 +1,24 @@
 
 val greetStrings = new Array[String](3)
 
+//Scala arrays are accessed by placing the index inside parens, not brackets
 greetStrings(0) = "Hello"
 greetStrings(1) = ", "
 greetStrings(2) = "world!\n"
 
+/*
+  Note, 'to' in the for loop is a method that takes one int argument.
+*/
 for (i <- 0 to 2)
   print(greetStrings(i))
+
+//More explicit version
+
+val explicitGreetStrings: array[String] = new Array[String](3)
+
+explicitGreetStrings.update(0, "Hello")
+explicitGreetStrings.update(1, ", ")
+explicitGreetStrings.update(2, "world!\n")
+
+for (i <- 0.to(2))
+  print(explicitGreetStrings.apply(i))
