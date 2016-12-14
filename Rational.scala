5ec6be135ac22d2d9e098f04b0b6c3291aacdbcf
Connor Lewellyn <lewellync@gmail.com>
2016-12-14 00:08:52 -0500

    Finished override and precondition checking through requires

--- Rational.scala
+++ Rational.scala
@@ -7,5 +7,20 @@
     but in Scala, the compiler does this for you. Magic.
 */
 class Rational(n: Int, d: Int) {
-  println("Created " + n + "/" + d)
+
+  //println("Created " + n + "/" + d)
+
+  /*
+    How about we override the toString method, instead of printing it out?
+    This way, when we create a new Rational in the console, Scala prints out
+    something a little more readable.
+  */
+  override def toString = n + "/" + d
+
+  /*
+    Next step is checking for preconditions. A valid rational with our limited
+    constructor could end up as 5/0, which is impossible. We use a require to
+    put a prcondition on the object. A require method takes one boolean param.
+  */
+  require (d != 0)
 }
