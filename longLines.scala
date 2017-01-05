43830364e7cf699bc2ea0898a80209f5332dff35
Connor Lewellyn <lewellync@gmail.com>
2017-01-04 23:37:45 -0500

    Finished 8.2

--- longLines.scala
+++ longLines.scala
@@ -2,8 +2,7 @@ import scala.io.Source
 
 // Methods exist as functions inside of objects, just like in Java. Functional
 // programming also wants us to make many small methods to be building blocks
-// for our programs. This is fine until we fill up possible namespace. To combat
-// this, Scala offers us local methods (methods declared within methods).
+// for our programs. This is fine until we fill up possible namespace.
 
 object LongLines {
 
@@ -22,3 +21,26 @@ object LongLines {
 // Totally unrelated to what we're doing, but remember that when you try to run
 // Scala programs, the current directory is not in the CLASSPATH so you need to
 // add '-cp .' after 'scala' when running a file.
+
+// After compiling, run with 'scala FindLongLines -cp . 45 LongLines.scala'
+
+// Scala offers the private method for functions, same as Java, so as to not
+// use too many names, but they also allow us to create local functions.
+
+object LocalLongLiens {
+
+  def processFile(filename: String, width: Int) = {
+
+    // Method declared within another method, only exists in this scope. Note,
+    // local functions can see parameters of their enclosing function.
+    def processLine(line: String) = {
+      if (line.length > width)
+      println(filename + ": " + line.trim)
+    }
+
+    val source = Source.fromFile(filename)
+    for (line <- source.getLines()) {
+      processLine(line)
+    }
+  }
+}
