4bcef25e90c8a146a37a16cf1517ff0c236cdddc
Connor Lewellyn <lewellync@gmail.com>
2017-02-03 14:56:21 -0500

    Starting work on stacking trats

--- Chapter 12 - Traits/IntQueue.scala
+++ Chapter 12 - Traits/IntQueue.scala
@@ -24,17 +24,40 @@ class BasicIntQueue extends IntQueue {
 val queue = new BasicIntQueue
 queue.put(10)
 queue.put(20)
-println(queue.get())
-println(queue.get())
+queue.get()
+queue.get()
 
 /*
   Now let's modify the behavior, not yet stacking traits, but applying traits
   that modify the default behavior of our queue. Note the super call in the trait
   Doubling that extends an abstract class. This is illegal for normal classes to
   do because they will fail at runtime, but because super calls in traits are
-  dynamically bound, the call will work as long as Doubling is mixed in to
-  another trait or class that gives a concrete definition to the method 'put'
+  dynamically bound, the call will work as long as Doubling is mixed in AFTER
+  another trait or class that gives a concrete definition to the method.
 */
 trait Doubling extends IntQueue {
+  // Abstract override is necessary when a trait defines a member that only works
+  // when mixed into some class that has a concrete definition of the method in
+  // question.
   abstract override def put(x: Int) = { super.put(2 * x) }
+}
+
+class MyQueue extends BasicIntQueue with Doubling
+val queue = new MyQueue
+queue.put(10)
+queue.get()
+
+// MyQueue defines no new code, just identifies a class and mixes in a trait.
+// The same could be done by suppling them to new instead like below:
+val queue = new BasicIntQueue with Doubling
+
+/*
+  So we've seen traits that modify default behavior. Now let's stack them. The
+  below traits have behavior that could change.
+*/
+trait Incrementing extends IntQueue {
+  abstract override def put(x: Int) = { super.put(x + 1) }
+}
+trait Filtering extends IntQueue {
+  abstract override def put(x: Int) = { if (x >= 0) super.put(x) }
 }
