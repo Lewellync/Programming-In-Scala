2bf11fc042d7f6326c8e09e4036e60aeabd2ec38
Connor Lewellyn <lewellync@gmail.com>
2017-02-05 15:12:28 -0500

    T R A I T S T A C K I N G

--- Chapter 12 - Traits/IntQueue.scala
+++ Chapter 12 - Traits/IntQueue.scala
@@ -53,11 +53,49 @@ val queue = new BasicIntQueue with Doubling
 
 /*
   So we've seen traits that modify default behavior. Now let's stack them. The
-  below traits have behavior that could change.
+  below traits have behavior that could change depending on which is applied first,
+  but they both make calls to super. As we said before, super is dynamically bound,
+  so there is an order. If Incrementing is first, then Filtering's super.put is
+  really a call to Incrementin's put, and Incrementing's put is a call to the actual
+  super class BasicIntQueue.
 */
 trait Incrementing extends IntQueue {
   abstract override def put(x: Int) = { super.put(x + 1) }
 }
+
 trait Filtering extends IntQueue {
   abstract override def put(x: Int) = { if (x >= 0) super.put(x) }
 }
+
+/*
+  In q1, put calls filtering first, which checks if the value is non-negative and
+  then calls it's super which would be incrementing. The queue contains 1 and 2,
+  but not 0 as -1 is checked by filtering first, and not passed up to incrementing.
+*/
+val q1 = new BasicIntQueue with Incrementing with Filtering
+q1.put(-1)
+q1.put(0)
+q1.put(1)
+q1.get()
+q1.get()
+
+/*
+  In q2 the opposite order of operations occurs with regards to the traits. Put calls
+  incrementing, which adds 1 to each argument and calls its super which would be
+  filtering. Since -1 has already been incremented to 0, filtering's put does not
+  catch it and it is passed along to its super, which is the BasicIntQueue put.
+*/
+val q2 = new BasicIntQueue with Filtering with Incrementing
+q2.put(-1)
+q2.put(0)
+q2.put(1)
+q2.get()
+q2.get()
+q2.get()
+
+/*
+  The ordering of the mixins is important, as you can see above, and not just for
+  the implementation of 'super'. The general rule is, the further mixins to the right
+  take effect first. If that method calls super, it invokes the method in the next
+  trait to the left, and so forth.
+*/
