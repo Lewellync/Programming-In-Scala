5a9c766ae93b290976e3380ea23fcfb8161a38fd
Connor Lewellyn <lewellync@gmail.com>
2017-02-13 18:09:36 -0500

    13.5

--- Chapter 13 - Packages and Imports/access_in_packages.scala
+++ Chapter 13 - Packages and Imports/access_in_packages.scala
@@ -22,8 +22,8 @@ class Outer {
 /*
   Access to protected members in Scala is also more restrictive than in Java. In Scala,
   a protected member is only accessible from sublcasses of the class the member is
-  located in. In 13.11, the access to f in Sub is OK, because f is delcared protected
   in Super and Sub is a subclass of Super. By contrast, the f in Other is not permitted
+  located in. In 13.11, the access to f in Sub is OK, because f is delcared protected
   because Other does not inherit from Super.
 */
 
@@ -38,4 +38,55 @@ package p {
   class Other {
     (new Super).f() // Error: f is not accessible
   }
+}
+
+/*
+  Scala has no exlicit modifier for public members; Any member not labeleled private
+  or protected is public. Public members can be accessed from anywhere.
+
+  Access modifiers in Scala can be augmented with qualifiers. A modifiers of the form
+  private[X] or protected[X] means that acess is private or protected "up to" X, where
+  X designates some enclosing package, object or singleton object. Qualified access
+  modifers give you very fine-grained control over visibility. In particular, they
+  enable you to express Java's accessibility notions which are not available in Scala's
+  default.
+
+  13.12 presents an example with many access qualifiers being used for a myriad of
+  different effects!
+*/
+
+// 13.12 Flexiblw scope of protection with access qualifiers
+package bobsrockets
+package navigation {
+  private[bobsrockets] class Navigator { // Access wihtin outer package
+    protected[navigation] def useStarChar() = {} // Same as packave visibility in Java
+    class LegOfJourney {
+      private[Navigator] val distance = 100 // Same as private in Java
+    }
+    private[this] var speed = 200 // Access only from the same object
+  }
+}
+package launch {
+  import navigation._
+  object Vehicle {
+    private[launch] val guide = new Navigator // Same as private in Java
+  }
+}
+private[LegOfJourney] // Same as private in Scala
+
+// 13.13 - Accessing private members of companion classes and objects
+class Rocket {
+  import Rocket.fuel
+  private def canGoHomeAgain = fuel > 20
+}
+object Rocket {
+  private def fuel = 10
+  def chooseStrategy(rocket: Rocket) = {
+    if(rocket.canGoHomeAgain)
+      goHome()
+    else
+      pickAStar()
+  }
+  def goHome() = {}
+  def pickAStar() = {}
 }
