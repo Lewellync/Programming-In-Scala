2374d7b7edaa817bc775d164972bd93300e9078c
Connor Lewellyn <lewellync@gmail.com>
2017-02-24 15:27:30 -0500

    15.2

--- Chapter 15 - Case Classes and Pattern Matching/listing.scala
+++ Chapter 15 - Case Classes and Pattern Matching/listing.scala
@@ -54,8 +54,66 @@ expr match {
 // 15.6.1 - Scala uses upper case to signify constants in pattern matching
 import math.{E, Pi}
 E match {
-  case Pi => "strange math? Pi = " + Pi
-  case _ => "OK"
+  case Pi => "strange math? Pi = " + Pi // It'll never hit this, since it's uppercase
+  case pi => "this works though" // It'll hit this, since it's not a constant
 }
 
-// 15.6.2 -
+// 15.7 - A pattern match with a constructor pattern
+expr match {
+  case BinOp("+", e, Number(0)) => println("a deep match")
+  case _ =>
+}
+
+// 15.8 - A sequence pattern with a fixed length
+expr match {
+  case List(0, _, _) => println("found it")
+  case _ =>
+}
+
+// 15.8.1 - Match a bunch of different things look at that nifty thing
+expr match {
+  case List(0, 1, a, _*) => println("we did it reddit " + a)
+  case _ =>
+}
+
+// 15.9 - A sequence pattern with an arbitrary length
+expr match {
+  case List(0, _*) => println("found it")
+  case _ =>
+}
+
+// 15.10 - A pattern match with a tuple pattern
+def tupleDemo(expr: Any) =
+  expr match {
+    case (a, b, c) => println("matched " + a + b + c)
+    case _ =>
+  }
+
+// 15.11 - A pattern match with typed patterns
+def generalSize(x: Any) = x match {
+  case s: String => s.length
+  case m: Map[_, _] => m.size
+  case _ => -1
+}
+
+// 15.11.1
+expr.isInstanceOf[String]
+expr.asInstanceOf[String]
+
+// 15.12 - Using type testing and casing instead of pattern matching
+if (x.isInstanceOf[String]) {
+  val s = x.asInstanceOf[String]
+  s.length
+} else ...
+
+// 15.12.1 - This does not work as expected, will return true for all maps
+def isIntIntMap(x: Any) = x match {
+  case m: Map[Int, Int] => true
+  case _ => false
+}
+
+// 15.13 - A pattern with a variable binding
+expr match {
+  case UnOp("abs", e @ UnOp("abs", _)) => e
+  case _ =>
+}
