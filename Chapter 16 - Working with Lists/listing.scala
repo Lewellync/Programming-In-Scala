6868a4a26720ff78b459bb0c2012f73384305522
Connor Lewellyn <lewellync@gmail.com>
2017-04-17 23:52:31 -0400

    Finished 16.6

--- Chapter 16 - Working with Lists/listing.scala
+++ Chapter 16 - Working with Lists/listing.scala
@@ -109,3 +109,40 @@ zipped.unzip
 abcde.toString // = List(a,b,c,d,e)
 abcde.mkString // = abcde
 abcde.mkString("[", ",", "]")
+val buf = new StringBuilder
+abcde addString (buf, "(", ";", ")")
+
+// 16.6.12 - iterator, toArray, copyToArray
+val arr = abcde.toArray
+arr.toList
+xs copyToArray (arr, start)
+
+// 16.6.13
+val arr2 = new Array[Int](10)
+List(1,2,3) copyToArrray (arr2, 3)
+
+// 16.1 - A merge sort function for Lists
+def msort[T](less: (T, T) => Boolean)(xs: List[T]): List[T] = {
+  def merge(xs: List[T], ys: List[T]): List[T] = {
+    (xs, ys) match {
+      case (Nil, _) => ys
+      case (_, Nil) => xs
+      // If the first element of xs is smaller, remove it from the xs and repeat
+      // the process on xs1 (the remainder) and y. Vice-versa.
+      case (x :: xs1, y :: ys1) =>
+        if (less(x, y)) x :: merge(xs1, ys)
+        else y :: merge(xs, ys1)
+    }
+  }
+
+  val n = xs.length / 2
+  if (n == 0) xs
+  else {
+    val (ys, zs) = xs splitAt n
+    merge(msort(less)(ys), msort(less)(zs))
+  }
+}
+
+val intSort = msort((x: Int, y: Int) => x < y) _ // Underscore stands for a missing
+                                                 // argument list
+val reverseIntSort = msort((x: Int, y: Int) => x > y) _
